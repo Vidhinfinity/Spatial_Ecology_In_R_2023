4b75e78864d555afc8da2a287e0ecb50ecb06743
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2023-10-03 17:26:29 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,16 @@
+# Here I can write anything I want! 
+
+# R as a calculator
+2 + 3
+
+# Assign to an object
+vidhi <- 2 + 3
+vidhi
+
+duccio <- 5 + 3
+duccio
+
+final <- vidhi * duccio
+final
+
+final^2
