09f7efeff85f47c1d95207a7a9e3252924f52185
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2023-10-03 17:52:33 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -14,3 +14,24 @@ final <- vidhi * duccio
 final
 
 final^2
+
+# array
+sophi <- c(10, 20, 30, 50, 70) # microplastics # functions have parentheses and inside them there are arguments
+
+paula <- c(100, 500, 600, 1000, 2000) # people
+
+Plot (paula, sophi)
+
+Plot (paula, sophi xlab= "number of people" , ylab= "microplastics" )
+
+people <- paula
+microplastics <- sophi
+
+plot(people, microplastics)
+
+plot (people, microplastics, pch= 19)
+
+plot (people, microplastics, pch= 19, cex=5)
+
+plot (people, microplastics, pch= 19, cex=5, col= "blue")
+
