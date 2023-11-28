ab1d2a8341ed8cc07a92b33783d0c26a28332999
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2023-11-28 16:50:02 +0100

    Create 08.Copernicus.Data.temperature.R

---
+++ 08.Copernicus.Data.temperature.R
@@ -0,0 +1,8 @@
+# Working on copernicus data. Discussion on copernicus site and definitions.
+#Leaf Area index: half the total area of green elements of the canopy per unit horizontal ground area.
+#FCOVER (Fraction of Vegetation Cover): the fraction of ground covered by green vegetation.
+#FAPAR (Fraction of Absorbed Photosynthetically Active Radiation): the fraction of the solar radiation absorbed by live leaves for the photosynthesis activity. 
+#NDVI: Normalized Difference Vegetation Index: NDVI = (REF_nir – REF_red)/(REF_nir + REF_red)
+#Dry Matter Productivity (DMP) represents the overall growth rate or dry biomass increase of the vegetation.
+#energy, watercycle and cryosphere parameters being discussed.
+
