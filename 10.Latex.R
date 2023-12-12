71857877810e94702b6efbe18e04da9464227536
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2023-12-12 18:09:49 +0100

    Create 10.Latex.R

---
+++ 10.Latex.R
@@ -0,0 +1,21 @@
+# why LaTeX
+
+##problem with word:
++ Formulas
++ Layout: header
++ Bibliography
+
+# Latex introduction
+#website: overleaf
+
+# backslash is used to start every line in LaTeX. In R, you use [] but in LaTeX we use graph parenthesis {}
+# % is the comment symbol in LaTex instead of #
+# in R there is library, in LaTeX we have use package
+# date with nothing inside {} , date will disappear
+# different sections can be added
+# no indent to remove indents from paragraphs
+# smallskip and bigskip to add line gaps
+# to change structure like fonts use quadratic parenthesis []
+
+#fromula in latex. begin and end equation. use function frac
+# for subscript _ ; for power ^ ; for multiplication symbol \times ; for square root  \sqrt {}. before argument of sq root put parenthesisi with 3 to get the cube root symbol.
