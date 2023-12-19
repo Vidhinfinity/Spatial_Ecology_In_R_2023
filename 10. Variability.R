85ebf46ddfbfe78cc1d231624325400c058cb309
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2023-12-19 17:53:24 +0100

    Create 10. Variability.R

---
+++ 10. Variability.R
@@ -0,0 +1,23 @@
+# measurements of RS based variability
+
+library(terra)
+library(imageRy)
+library(viridis)
+
+im.list()
+
+sent <- im.import("sentinel.png")
+im.plotRGB(sent, r= 1, g= 2, b= 3) #plotting the image
+#band 1 - NIR
+#band 2 - red
+#band 3 - green
+
+NIR <- sent [[1]]
+plot (NIR)
+
+#method called "moving window" is used to check variability. Function called focal is used in R. It is going to dp calculate standard deviation in our case.
+
+focal (NIR, matri) #nir is the band, matrix is describing the dimension of the moving window, composed of 9 pixels made of 3 * 3 dimensions. function is sd that is std deviation
+sd3 <- focal(NIR, matrix(1/9, 3, 3), fun=sd)
+plot(sd3)
+viridis <- colorRampPalette(viridis(7))(255) #variability in space #The “viridis” scales do better - they cover a wide perceptual range in brightness in brightness and blue-yellow.
