c0e1ead9d81751b01f316cc7534c29d368e15e72
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2023-12-21 17:19:52 +0100

    Update 10. Variability.R

--- 10. Variability.R
+++ 10. Variability.R
@@ -21,3 +21,17 @@ focal (NIR, matri) #nir is the band, matrix is describing the dimension of the m
 sd3 <- focal(NIR, matrix(1/9, 3, 3), fun=sd)
 plot(sd3)
 viridis <- colorRampPalette(viridis(7))(255) #variability in space #The “viridis” scales do better - they cover a wide perceptual range in brightness in brightness and blue-yellow.
+
+#plotting with 7*7 standard deviation
+sd7 <- focal(NIR, matrix(1/49, 7, 7), fun=sd)
+sd7
+plot(sd7)
+viridis <- colorRampPalette(viridis(7))(255) 
+plot(sd3, col=viridis)
+
+
+
+
+
+
+
