5f35aee520da13fb4b6e213682d1fea3b70a9bf4
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2024-08-06 23:38:10 +0200

    Update 04. remote.sensing.Visualisation.R

--- 04. remote.sensing.Visualisation.R
+++ 04. remote.sensing.Visualisation.R
@@ -1,16 +1,23 @@
-# This is a script to visualize satellite data
+# This script visualizes satellite data
 
-library(devtools) # packages in R are also called libraries
+# Load the devtools package, which is used to install packages from GitHub
+library(devtools) 
 
-# install the imageRy package from GitHub
+# Install the imageRy package from GitHub
 devtools::install_github("ducciorocchini/imageRy")
 
+# Load the imageRy package, which is used for handling satellite imagery
 library(imageRy)
+# Load the terra package, which is used for spatial data manipulation
 library(terra)
-# now we will use a package
+
+# List available images in the working directory
 im.list()
 
-# importing data
-# blue band
-b2 <- im.import("sentinel.dolomites.b2.tif") # b2 is the blue wavelength
+# Importing satellite data
+# Import the blue band (B2) from Sentinel-2 imagery
+b2 <- im.import("sentinel.dolomites.b2.tif") 
+# b2 represents the blue wavelength band from the Sentinel-2 satellite image
+
+# Display the imported blue band data
 b2
