1fe5c6fe4f3ea40ec697d339da78f17d341d0f6c
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2024-08-07 22:00:37 +0200

    Update 10. Variability.R

--- 10. Variability.R
+++ 10. Variability.R
@@ -1,37 +1,40 @@
 # measurements of RS based variability
 
-library(terra)
-library(imageRy)
-library(viridis)
+# Load necessary libraries
+library(terra)    # For raster data processing
+library(imageRy)  # For image import and manipulation
+library(viridis)  # For creating perceptually uniform color palettes
 
+# List available images in the workspace
 im.list()
 
+# Import the Sentinel image from the specified file
 sent <- im.import("sentinel.png")
-im.plotRGB(sent, r= 1, g= 2, b= 3) #plotting the image
-#band 1 - NIR
-#band 2 - red
-#band 3 - green
 
-NIR <- sent [[1]]
-plot (NIR)
+# Plot the image using RGB bands
+# Band 1 - NIR (Near-Infrared), Band 2 - Red, Band 3 - Green
+im.plotRGB(sent, r = 1, g = 2, b = 3) # Plotting the image
 
-#method called "moving window" is used to check variability. Function called focal is used in R. It is going to dp calculate standard deviation in our case.
+# Extract the NIR band from the Sentinel image
+NIR <- sent[[1]]
+plot(NIR)
 
-focal (NIR, matri) #nir is the band, matrix is describing the dimension of the moving window, composed of 9 pixels made of 3 * 3 dimensions. function is sd that is std deviation
-sd3 <- focal(NIR, matrix(1/9, 3, 3), fun=sd)
+# Method called "moving window" is used to check variability.
+# Function called focal is used in R to calculate standard deviation.
+# Correct focal call to calculate standard deviation using a 3x3 window
+sd3 <- focal(NIR, matrix(1/9, 3, 3), fun = sd)
 plot(sd3)
-viridis <- colorRampPalette(viridis(7))(255) #variability in space #The “viridis” scales do better - they cover a wide perceptual range in brightness in brightness and blue-yellow.
 
-#plotting with 7*7 standard deviation
-sd7 <- focal(NIR, matrix(1/49, 7, 7), fun=sd)
-sd7
+# Create a color palette using viridis
+viridis <- colorRampPalette(viridis(7))(255) # Variability in space
+
+# Plotting standard deviation with 3x3 window using viridis palette
+plot(sd3, col = viridis)
+
+# Calculate standard deviation using a 7x7 window
+sd7 <- focal(NIR, matrix(1/49, 7, 7), fun = sd)
 plot(sd7)
-viridis <- colorRampPalette(viridis(7))(255) 
-plot(sd3, col=viridis)
 
-
-
-
-
-
-
+# Plotting standard deviation with 7x7 window using viridis palette
+viridis <- colorRampPalette(viridis(7))(255)
+plot(sd7, col = viridis)
