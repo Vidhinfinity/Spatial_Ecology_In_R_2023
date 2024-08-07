a07a5f95968d8fa8b58e9b5f18b2eda02f76ef74
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2024-08-07 22:18:28 +0200

    Update 10.Latex.R

--- 10.Latex.R
+++ 10.Latex.R
@@ -10,7 +10,7 @@
 
 # backslash is used to start every line in LaTeX. In R, you use [] but in LaTeX we use graph parenthesis {}
 # % is the comment symbol in LaTex instead of #
-# In R there is the library, in LaTeX we have to use package
+# In R there is the library, in LaTeX we have to use the package
 # date with nothing inside {} , date will disappear
 # Different sections can be added
 # no indent to remove indents from paragraphs
@@ -20,3 +20,5 @@
 #formula in latex. begin and end equation. use function frac
 # for subscript _ ; for power ^ ; for multiplication symbol \times ; for square root  \sqrt {}. before an argument of sq root put parenthesis with 3 to get the cube root symbol.
 # working 
+
+
