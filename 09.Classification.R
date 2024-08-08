14192b00f09614b8f6f0e158259df0668d8939f6
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2024-08-08 16:25:37 +0200

    Update 09.Classification.R

--- 09.Classification.R
+++ 09.Classification.R
@@ -88,6 +88,16 @@ y2006 <- c(45, 55)
 tabout <- data.frame(class, y1992, y2006)
 tabout
 
+# building the output table
+cover <- c("forest", "agriculture") 
+perc1992 <- c(83.08, 16.91)
+perc2006 <- c(45.31, 54.69)
+
+# final table
+p <- data.frame(cover, perc1992, perc2006)
+p
+
+
 # final output
 p1 <- ggplot(tabout, aes(x=class, y=y1992, color=class)) + geom_bar(stat="identity", fill="white")
 p2 <- ggplot(tabout, aes(x=class, y=y2006, color=class)) + geom_bar(stat="identity", fill="white")
