0584f927bc7e2831d27168698aa6ac3387315f10
Vidhinfinity <146854752+Vidhinfinity@users.noreply.github.com>
2024-08-09 13:56:32 +0200

    Update Final.exam.R

--- Final.exam.R
+++ Final.exam.R
@@ -52,6 +52,7 @@ plot(mang2014[[1]])
 # Calculate and plot the Difference Vegetation Index (DVI)
 dvimang2014 <- mang2014[[1]] - mang2014[[2]] # Calculate DVI for 2014
 dvimang2014
+cl <-  colorRampPalette(c("red", "black", "green"))(100)
 plot(dvimang2014, col=cl, , main="DVI 2014") # Plot DVI for 2014
 
 # Calculate and plot DVI for 2016
@@ -95,6 +96,105 @@ print(paste("DVI reduction:", dvi_reduction, "%"))
 #results: "NDVI reduction: 87.1557142857143 %"
 # "DVI reduction: 87.27 %"
 
+##Variability
+
+# Plot the image using RGB bands
+im.plotRGB(mang2014, r = 1, g = 2, b = 3) # Plotting the image
+
+# Extract the NIR band from the Sentinel image
+NIR2014 <- mang2014[[1]]
+plot(NIR2014, col=cl)
+
+# Method called "moving window" is used to check variability.
+# Function called focal is used in R to calculate standard deviation.
+# calculate standard deviation using a 3x3 and 7x7 window
+sd3 <- focal(NIR2014, matrix(1/9, 3, 3), fun = sd)
+sd7 <- focal(NIR2014, matrix(1/9, 7, 7), fun = sd)
+
+# Create a color palette using viridis
+viridis <- colorRampPalette(viridis(7))(255) # Variability in space
+plot(sd7, col=viridis)
+plot(sd3, col=viridis)
+
+# principal component analysis
+
+# Pairs function is used to make a plot to see correlation. 1 is a perfect positive correlation and -1 is a negative correlation.
+# Perform PCA 
+mang2014.pca <- im.pca(mang2014)
+
+# Isolating PC1
+pc1 <- mang2014.pca$PC1
+plot(pc1) # Plot PC1
+
+# Calculation of standard deviation on top of PC1 using a 3x3 moving window
+pc1sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
+plot(pc1sd3, col=viridis) # Plot with viridis color scale
+
+# Calculation of standard deviation on top of PC1 using a 7x7 moving window
+pc1sd7 <- focal(pc1, matrix(1/49, 7, 7), fun=sd)
+plot(pc1sd7, col=viridis) # Plot with viridis color scale
+
+
+# Plot the standard deviation from the variability script
+plot(sd3, col=viridis)
+plot(sd7, col=viridis)
+plot(pc1, col=viridis)
+plot(pc1sd3, col=viridis)
+plot(pc1sd7, col=viridis)
+
+# Stack all standard deviation layers
+sdstack <- c(sd3,sd7,pc1sd3, pc1sd7)
+plot(sdstack, col=viridis) # Plot the stack with viridis color scale
+
+# Assign names to the layers in the stack
+names(sdstack) <- c("sd3","sd7","pc1sd3", "pc1sd7")
+plot(sdstack, col=viridis) # Plot the named stack with viridis color scale
+
+#calculation of variability and pca for 2016
+
+# Plot the image using RGB bands
+im.plotRGB(mang2016, r = 1, g = 2, b = 3) # Plotting the image
+
+# Extract the NIR band from the Sentinel image
+NIR2016 <- mang2016[[1]]
+plot(NIR2016, col=cl)
+
+SD3 <- focal(NIR2016, matrix(1/9, 3, 3), fun = sd)
+SD7 <- focal(NIR2016, matrix(1/9, 7, 7), fun = sd)
+
+plot(SD7, col=viridis)
+plot(SD3, col=viridis)
+
+#PERFORM PCA
+mang2016.pca <- im.pca(mang2016)
+
+# Isolating PC1
+PC1 <- mang2016.pca$PC1
+plot(PC1) # Plot PC1
+
+# Calculation of standard deviation on top of PC1 using a 3x3 moving window
+PC1SD3 <- focal(PC1, matrix(1/9, 3, 3), fun=sd)
+plot(PC1SD3, col=viridis) # Plot with viridis color scale
+
+# Calculation of standard deviation on top of PC1 using a 7x7 moving window
+PC1SD7 <- focal(PC1, matrix(1/49, 7, 7), fun=sd)
+plot(pc1sd7, col=viridis) # Plot with viridis color scale
+
+
+# Plot the standard deviation from the variability script
+plot(SD3, col=viridis)
+plot(SD7, col=viridis)
+plot(PC1, col=viridis)
+plot(PC1SD3, col=viridis)
+plot(PC1SD7, col=viridis)
+
+sdstack1 <- c(SD3,SD7,PC1SD3, PC1SD7)
+plot(sdstack1, col=viridis) # Plot the stack with viridis color scale
+
+# Assign names to the layers in the stack
+names(sdstack1) <- c("SD3","SD7","PC1SD3", "PC1SD7")
+
+plot(sdstack1, col=viridis) # Plot the named stack with viridis color scale
 
 # Step 6: Perform PCA (Principal Component Analysis)
 # Perform PCA on 2014 and 2016 images
@@ -141,7 +241,7 @@ mang2016c <- im.classify(mang2016, num_clusters=2)
 mang2016c # Print classification result
 plot(mang2016c) # Plot the classified 2016 image
 
-# Step 12: Compare classifications
+# Step: Compare classifications
 par(mfrow=c(1, 2)) # Set up a 1x2 plot layout for side-by-side comparison
 plot(mang2014c) # Plot the classified 2014 image
 plot(mang2016c) # Plot the classified 2016 image
@@ -152,9 +252,9 @@ tot2014 <- ncell(mang2014c) # Total number of pixels in the 2014 image
 p2014 <- fmang2014c * 100 / tot2014 # Calculate percentage of each class
 p2014 # Print percentages for 2014
 
-layer        value   count
-1 0.0002040816 0.0002040816 85.0749
-2 0.0002040816 0.0004081633 14.9251
+##layer        value   count
+#1 0.0002040816 0.0002040816 85.0749
+#2 0.0002040816 0.0004081633 14.9251
 
 # Calculate frequency of each class for 2016
 fmang2016c <- freq(mang2016c) # Get frequency of classes in the 2016 image
@@ -162,9 +262,9 @@ tot2016 <- ncell(mang2016c) # Total number of pixels in the 2016 image
 p2016 <- fmang2016c * 100 / tot2016 # Calculate percentage of each class
 p2016 # Print percentages for 2016
 
-layer        value    count
-1 0.0002040816 0.0002040816 86.22653
-2 0.0002040816 0.0004081633 13.77347
+#layer        value    count
+#1 0.0002040816 0.0002040816 86.22653
+#2 0.0002040816 0.0004081633 13.77347
 
 # Build final output table
 class <- c("water", "forest") # Define class names
@@ -173,9 +273,9 @@ y2016 <- c(87, 13) # Percentage of each class in 2016
 tabout <- data.frame(class, y2014, y2016) # Create a data frame with the results
 tabout
 
-class y2014 y2016
-1  water    85    87
-2 forest    15    13
+#class y2014 y2016
+#1  water    85    87
+#2 forest    15    13
 
 # Build a summary table for visualization
 cover <- c("water", "forest")
